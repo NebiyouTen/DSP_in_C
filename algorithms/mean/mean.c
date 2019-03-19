1e512dc9551fb16b214209ed0846faba04bfaa18
NebiyouTen <nebiyoutenager@gmail.com>
2019-03-19 16:35:34 -0600

    mean

---
+++ algorithms/mean/mean.c
@@ -0,0 +1,25 @@
+/*
+ * A simple program to get the mean of a signal
+ */
+
+#include "waveforms.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+double calculate_mean(double *input_signal, int length);
+
+int main(){
+
+  double mean;
+  printf("SIze of InputSignal_f32_1kHz_15kHz is %lu \n", sizeof(InputSignal_f32_1kHz_15kHz)/sizeof(double) );
+  mean = calculate_mean(InputSignal_f32_1kHz_15kHz, SIGNAL_SIZE_1);
+  printf("Mean= %f \n", mean);
+}
+
+double calculate_mean(double *input_signal, int length){
+  int i ;
+  double mean = 0;
+  for (i =0; i < length; i++)
+    mean += input_signal[i];
+  return mean / length;
+}
